488b35e241cc1278f3dcebd46691f090fc61e5ed
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-05-19 14:41:55 -0500

    finished music

--- music.sql
+++ music.sql
@@ -37,23 +37,22 @@ CREATE TABLE songs
   title TEXT NOT NULL,
   duration_in_seconds INTEGER NOT NULL,
   release_date DATE NOT NULL,
-  album INT REFERENCES albums,
-  producers TEXT[] NOT NULL
+  album INT REFERENCES albums
 );
 
 INSERT INTO songs
-  (title, duration_in_seconds, release_date, album, producers)
+  (title, duration_in_seconds, release_date, album)
 VALUES
-  ('MMMBop', 238, '04-15-1997', 1, '{"Dust Brothers", "Stephen Lironi"}'),
-  ('Bohemian Rhapsody', 355, '10-31-1975', 2, '{"Roy Thomas Baker"}'),
-  ('One Sweet Day', 282, '11-14-1995', 3, '{"Walter Afanasieff"}'),
-  ('Shallow', 216, '09-27-2018', 4, '{"Benjamin Rice"}'),
-  ('How You Remind Me', 223, '08-21-2001', 5, '{"Rick Parashar"}'),
-  ('New York State of Mind', 276, '10-20-2009', 6, '{"Al Shux"}'),
-  ('Dark Horse', 215, '12-17-2013', 7, '{"Max Martin", "Cirkut"}'),
-  ('Moves Like Jagger', 201, '06-21-2011', 8, '{"Shellback", "Benny Blanco"}'),
-  ('Complicated', 244, '05-14-2002', 9, '{"The Matrix"}'),
-  ('Say My Name', 240, '11-07-1999', 10, '{"Darkchild"}');
+  ('MMMBop', 238, '04-15-1997', 1),
+  ('Bohemian Rhapsody', 355, '10-31-1975', 2),
+  ('One Sweet Day', 282, '11-14-1995', 3),
+  ('Shallow', 216, '09-27-2018', 4),
+  ('How You Remind Me', 223, '08-21-2001', 5),
+  ('New York State of Mind', 276, '10-20-2009', 6),
+  ('Dark Horse', 215, '12-17-2013', 7),
+  ('Moves Like Jagger', 201, '06-21-2011', 8),
+  ('Complicated', 244, '05-14-2002', 9),
+  ('Say My Name', 240, '11-07-1999', 10);
 
 CREATE TABLE songs_artists (
   id SERIAL PRIMARY KEY,
@@ -70,5 +69,5 @@ CREATE TABLE songs_producers (
   producer_id INT REFERENCES producers
 );
 
--- INSERT INTO songs_producers (song_id, producer_id)
--- VALUES FINISH
+INSERT INTO songs_producers (song_id, producer_id)
+VALUES (1, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (7, 9), (8, 10), (8, 11), (9, 12), (10, 13);
